655b2f3591d48bb7a53fb223a87e30247c225f63
LeeSH <whsqkaak@naver.com>
2019-05-07 17:22:08 +0900

    add conditional webpage in main.js

--- web1_html_internet-master/web1_html_internet-master/main.js
+++ web1_html_internet-master/web1_html_internet-master/main.js
@@ -1,15 +1,16 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-
+ 
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    var pathName = url.parse(_url, true).pathname;
-    var title = queryData.id;
+    var pathname = url.parse(_url, true).pathname;
     
-    if(pathName == '/'){
-      fs.readFile(`../data/${queryData.id}`, 'utf8', function(err, description){
+    if(pathname === '/'){
+      if(queryData.id === undefined){
+        var title = 'Welcome';
+        var description = 'Hello, Node.js';
         var template = `
         <!doctype html>
         <html>
@@ -29,13 +30,36 @@ var app = http.createServer(function(request,response){
         </body>
         </html>
         `;
-        response.writeHead(200);
-        response.end(template);
-      })
-    } else{
+      
+      } else {
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+          var title = queryData.id;
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+        });
+      }
+      response.writeHead(200);
+      response.end(template);
+    } else {
       response.writeHead(404);
-      response.end('Not Found');
+      response.end('Not found');
     }
-
 });
 app.listen(3000);
